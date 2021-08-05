dd1489eb0f06ac6a6879a86af1382c8d8858ccef
Crowdbotics <team@crowdbotics.com>
2021-08-05 00:29:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "hidden_dew_29483Identifier",
+  url: "https://hidden_dew_29483Identifier.botics.co",
+  api: "https://hidden_dew_29483Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
